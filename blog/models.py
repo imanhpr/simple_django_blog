832339b1a501062016571ab04683dbaf5a2f353f
Iman Hosseini Pour <imanhpr1999@gmail.com>
2021-11-18 13:52:45 +0330

    ADD: add a custom manager for Post model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-from django.db.models.enums import Choices
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManger(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -16,7 +20,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
-
+    objects = models.Manager()
+    published = PublishedManger()
     class Meta:
         ordering = ("-publish",)
 

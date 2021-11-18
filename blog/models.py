5cd35a172e06cf2aa05422d3c0693a8e576d801c
Iman Hosseini Pour <imanhpr1999@gmail.com>
2021-11-18 14:42:21 +0330

    ADD: add canonical url to the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
-from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db import models
+from django.urls.base import reverse
+from django.utils import timezone
 
 
 class PublishedManger(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status='published')
+        return super().get_queryset().filter(status="published")
 
 
 class Post(models.Model):
@@ -22,8 +23,15 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
     objects = models.Manager()
     published = PublishedManger()
+
     class Meta:
         ordering = ("-publish",)
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
 
     def __str__(self) -> str:
         return self.title

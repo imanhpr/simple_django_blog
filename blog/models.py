3a2c2a935b802ab2d0de88ff742dafcb5a463567
Iman Hosseini Pour <imanhpr1999@gmail.com>
2021-11-18 11:35:10 +0330

    add post model

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.enums import Choices
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -6,16 +7,18 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     STATUS_CHOICES = (("draft", "Draft"), ("published", "Published"))
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250 , unique_for_date='publish')
-    author = models.ForeignKey(User , on_delete=models.CASCADE , related_name='blog_posts')
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10)
+    status = models.CharField(max_length=10, Choices=STATUS_CHOICES, default="draft")
 
     class Meta:
-        ordering = '-publish' ,
+        ordering = ("-publish",)
 
     def __str__(self) -> str:
         return self.title

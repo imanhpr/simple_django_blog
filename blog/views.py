80d21a33e2060c412fd4fc55a28b1f6227d5fffe
Iman Hosseini Pour <imanhpr1999@gmail.com>
2021-11-18 16:21:47 +0330

    ADD: add simple paginator

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from typing import NewType
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
@@ -9,13 +10,27 @@ SlugType = NewType("SlugType", str)
 
 
 def post_list(request: HttpRequest) -> HttpResponse:
-    posts = Post.published_manager.all()
+    objects = Post.published_manager.all()
+    print(len(objects))
+    paginator = Paginator(object_list=objects, per_page=1)
+    
+    page_number = request.GET.get("page")
+    try:
+        posts = paginator.page(page_number)
+        print(posts)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
     return render(
-        request=request, template_name="blog/post/list.html", context={"posts": posts}
+        request=request,
+        template_name="blog/post/list.html",
+        context={"page": posts},
     )
 
 
-def post_detail(request: HttpRequest, year: int, month: int, day: int, post : SlugType):
+def post_detail(request: HttpRequest, year: int, month: int, day: int, post: SlugType):
     post = get_object_or_404(
         Post,
         slug=post,
